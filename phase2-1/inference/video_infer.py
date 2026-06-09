e5101701b571ca5a8ed3e669107fcec016dfc27d
QQ-1223 <1502585272@qq.com>
2026-06-09 17:56:31 +0800

    Delete phase2-1 directory

--- phase2-1/inference/video_infer.py
+++
@@ -1,9 +0,0 @@
-from ultralytics import YOLO
-
-#加载训练好的模型
-model = YOLO("./weights/best.pt")
-#测试视频
-video_path = "./test.mp4"
-#推理
-model.predict(source=video_path,save=True,save_conf=True,conf=0.25)
-
--- phase2-1/train_config/train.sh
+++
@@ -1,1 +0,0 @@
-python train.py --data bvn.yaml --epochs 30
